30fb456c31ffdda3dab50159a1bb91bed07707ea
Matias Choquet <chm5250a@ensfsi.onmicrosoft.com>
2025-03-04 17:47:33 +0100

    tp2

---
+++ BacASable/src/tableaux/TestTableau.java
@@ -0,0 +1,18 @@
+package tableaux;
+
+public class TestTableau {
+
+	public static void main(String[] args) {
+		int[] tableauEntier = { 5, 2, 7, 5, 3 };
+		String[] tableauChaine = new String[5];
+		int nbChaine = 0;
+		tableauChaine[nbChaine] = "Hello";
+		nbChaine++;
+		tableauChaine[nbChaine] = "World";
+		nbChaine++;
+		tableauChaine[3] = " !";
+
+		System.out.println(tableauEntier);
+
+	}
+}
